49e002d8d5a3228034b6c6f71a066cb236fa64e4
sbhanderi18 <shubham@logicwind.com>
2020-07-05 23:44:21 +0530

    build

---
+++ build/precache-manifest.588958daa22a27043f5ecf8b85ec007a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61cd6b891cbd23f1317c743f8f3b7d08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7a00be3312e1f07936a",
+    "url": "/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "a17b7f5efc6a2a04df09",
+    "url": "/static/css/main.77a5d7cb.chunk.css"
+  },
+  {
+    "revision": "e7a00be3312e1f07936a",
+    "url": "/static/js/2.3ae0e537.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3ae0e537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a17b7f5efc6a2a04df09",
+    "url": "/static/js/main.2af82301.chunk.js"
+  },
+  {
+    "revision": "ecc0c871da8f2c1f008e",
+    "url": "/static/js/runtime-main.03578b13.js"
+  }
+]);
